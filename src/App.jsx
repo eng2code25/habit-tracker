e306077437f9000e38ef65385f5ede8bbdea8ad4
Chee Kit <145242543+eng2code25@users.noreply.github.com>
2026-06-14 18:24:58 +0800

    Update App.jsx
    Added habit input field.

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  {
+    /*create habits drop down table*/
+  }
+  const [inputTable, setInputTable] = useState(false);
+
+  {
+    /*habit input*/
+  }
+  const [inputHabit, setInputHabit] = useState("");
+
+  {
+    /*Overall data*/
+  }
+  const [summaryHabit, setSummaryHabit] = useState([]);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+    <section>
+      <h1>My Habit Tracker</h1>
+      <button onClick={() => setInputTable(true)}>Create Habits</button>
+      {inputTable && (
+        <form className="input-form">
+          <div className="input-habit">
+            <label>Habit: </label>
+            <input
+              placeholder="Enter habit..."
+              value={inputHabit}
+              onChange={(e) => setInputHabit(e.target.value)}
+            ></input>
+          </div>
+          <div className="input-date">
+            <label>Date: </label>
+            <input type="date"></input>
+          </div>
+          <div className="input-note">
+            <label>Note: </label>
+            <div>
+              <textarea className="note-text"></textarea>
+            </div>
+          </div>
+          <div className="button-input">
+            <div>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  habit: inputHabit;
+                }}
+              >
+                Save
+              </button>
+            </div>
+            <div>
+              <button onClick={() => setInputTable(false)}>Close</button>
+            </div>
+          </div>
+        </form>
+      )}
+      {/*Display UI section*/}
+      <section>
+        <h2>Saved Habits</h2>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
           <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
+            {summaryHabit.map((item, index) => {
+              <li className="habit-card" key={index}>
+                <span>Habit: {item.habit}</span>
+              </li>;
+            })}
           </ul>
         </div>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
